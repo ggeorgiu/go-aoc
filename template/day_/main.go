47c2d2be2db42c0ce5edc7fb22f3893aa1e75bc0
Gabriel Georgiu <gabriel.georgiu@gmail.com>
2023-11-11 16:32:07 +0200

    Add generate script (#4)
    * Add 2015 day 1
    
    * Restructure project
    
    * Add generate script

---
+++ template/day_/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"go-aoc/pkg/in"
+)
+
+func main() {
+	input := in.ReadFileAsString("/year_/day_/part1.test")
+	fmt.Printf("P2: %s", part1(input))
+}
+
+func part1(input string) string {
+	return ""
+}
+
+func part2(input string) string {
+	return ""
+}
