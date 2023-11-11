f9d4f53ed44dab0ab003defd6ea70712cc27af0b
ggeorgiu <ggeorgiu@uw.co.uk>
2023-11-11 12:04:52 +0200

    Add 2015 day 1

--- 2015/day1/main.go
+++ 2015/day1/main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world")
+	input := ReadFileAsString("/day1/part1.input")
+	fmt.Println(part1(input))
+	fmt.Println(part2(input))
+}
+
+func part1(input string) int {
+	var total int
+	for _, r := range input {
+		switch r {
+		case '(':
+			total++
+		case ')':
+			total--
+		}
+	}
+
+	return total
+}
+
+func part2(input string) interface{} {
+	var c int
+	var p int
+	for i, r := range input {
+		switch r {
+		case '(':
+			c++
+		case ')':
+			c--
+		}
+
+		if c == -1 {
+			p = i + 1
+			break
+		}
+	}
+
+	return p
+}
+
+func ReadFileAsString(filePath string) string {
+	path, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	dat, err := os.ReadFile(path + filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(dat)
 }
