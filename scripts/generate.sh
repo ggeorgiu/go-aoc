0d1baaa14b20c4c6982d33fb266ee74b29512309
Gabriel Georgiu <gabriel.georgiu@gmail.com>
2023-11-29 17:48:05 +0200

    Update generation script (#10)

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,6 +2,7 @@
 
 DAY=$1
 YEAR=2015
+
 if [ -n "$2" ]; then
   YEAR=$2
 
@@ -16,15 +17,15 @@ TEMPLATE_FOLDER=template
 INPUT_FILE=part1.input
 TEST_INPUT_FILE=part1.test
 
-cpath="$(pwd)/$YEAR/$TEMPLATE_PREFIX"
-template_path="$(pwd)/$TEMPLATE_FOLDER/$TEMPLATE_PREFIX"
+T_PATH="$(pwd)/$TEMPLATE_FOLDER/$TEMPLATE_PREFIX$TEMPLATE_POSTFIX"
+G_PATH="$(pwd)/$YEAR/$TEMPLATE_PREFIX$DAY"
 
-if [ ! -d "$cpath$DAY" ]; then
-  cp -R "$template_path$TEMPLATE_POSTFIX" "$cpath$DAY"
-  find "$cpath$DAY" -name "*.go" | xargs sed -i '' "s/day_/day$DAY/g"
-  find "$cpath$DAY" -name "*.go" | xargs sed -i '' "s/year_/$YEAR/g"
-  touch "$cpath$DAY/$INPUT_FILE"
-  touch "$cpath$DAY/$TEST_INPUT_FILE"
+if [ ! -d "$G_PATH" ]; then
+  cp -R "$T_PATH" "$G_PATH"                                                       #copy template folder in a new day folder
+  find "$G_PATH" -name "*.go" -print0 | xargs -0 sed -i '' "s/day_/day$DAY/g"     #replace day placeholders with the current day
+  find "$G_PATH" -name "*.go" -print0 | xargs -0 sed -i '' "s/year_/$YEAR/g"      #replace year placeholders with the current year
+  touch "$G_PATH/$INPUT_FILE"                                                     #add input file
+  touch "$G_PATH/$TEST_INPUT_FILE"                                                #add test input file
 else
   echo "Year $YEAR Day $DAY already exists."
 fi
