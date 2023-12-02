86cd77094908f940c0e408698162f4c3d50f9a7a
ggeorgiu <ggeorgiu@uw.co.uk>
2023-12-02 14:09:13 +0200

    Add 2023 day 2

---
+++ 2023/day2/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"go-aoc/pkg/in"
+)
+
+func main() {
+	input := in.ReadFileAsStringSlice("/2023/day2/part1.input")
+	fmt.Printf("P1: %v\n", part1(input))
+	fmt.Printf("P1: %v\n", part2(input))
+}
+
+func part1(input []string) int {
+	config := map[string]int{
+		"red":   12,
+		"green": 13,
+		"blue":  14,
+	}
+
+	sum := 0
+	for _, line := range input {
+		parts := strings.Split(line, ": ")
+		gameID, _ := strconv.Atoi(strings.TrimPrefix(parts[0], "Game "))
+		games := strings.Split(parts[1], "; ")
+
+		possible := true
+		for _, g := range games {
+			if !possible {
+				break
+			}
+
+			draws := strings.Split(g, ", ")
+			for _, d := range draws {
+				vals := strings.Split(d, " ")
+				c, _ := strconv.Atoi(vals[0])
+				color := vals[1]
+
+				if c > config[color] {
+					possible = false
+					break
+				}
+			}
+		}
+
+		if possible {
+			sum += gameID
+		}
+	}
+
+	return sum
+}
+
+func part2(input []string) int {
+	sum := 0
+	for _, line := range input {
+		parts := strings.Split(line, ": ")
+		games := strings.Split(parts[1], "; ")
+		m := map[string]int{
+			"red":   0,
+			"blue":  0,
+			"green": 0,
+		}
+
+		for _, g := range games {
+			draws := strings.Split(g, ", ")
+			for _, d := range draws {
+				vals := strings.Split(d, " ")
+				c, _ := strconv.Atoi(vals[0])
+				color := vals[1]
+
+				if m[color] < c {
+					m[color] = c
+				}
+			}
+		}
+
+		sum += m["red"] * m["blue"] * m["green"]
+	}
+
+	return sum
+}
