84ff028d53757d257df481d2dc6db842e9da9060
ggeorgiu <ggeorgiu@uw.co.uk>
2023-12-06 18:15:46 +0200

    Tidy up 2023 day 5

--- 2023/day5/main.go
+++ 2023/day5/main.go
@@ -11,11 +11,12 @@ import (
 )
 
 type interval struct {
-	start int
-	end   int
-	sv    int
+	start      int
+	end        int
+	startValue int
 }
 
+// Brute force
 func main() {
 	input := in.ReadFileAsStringSlice("/2023/day5/part1.input")
 	fmt.Printf("P1: %v\n", part1(input))
@@ -23,57 +24,26 @@ func main() {
 }
 
 func part1(input []string) interface{} {
-	m := make(map[string][]interval)
-	sm := make(map[int]int)
+	seeds, conf := parseSeedsAndConfig(input)
 
-	var seeds []int
-	to := ""
-	for _, v := range input {
-		if strings.Contains(v, "seeds:") {
-			parts := strings.Split(v, ": ")
-			for _, v := range strings.Split(parts[1], " ") {
-				is := conv.ToInt(v)
-				seeds = append(seeds, is)
-				sm[is] = is
-			}
-
-			continue
-		}
-		if strings.Contains(v, "map:") {
-			parts := strings.Split(v, " ")
-			ft := strings.Split(parts[0], "-to-")
-
-			to = ft[1]
-			continue
-		}
-
-		if v == "" {
-			continue
-		}
-
-		parts := strings.Split(v, " ")
-		destCoresp := conv.ToInt(parts[0])
-		sourceStart := conv.ToInt(parts[1])
-		size := conv.ToInt(parts[2])
-
-		m[to] = append(m[to], interval{sourceStart, sourceStart + size - 1, destCoresp})
+	workingMap := make(map[int]int)
+	for _, v := range seeds {
+		workingMap[v] = v
 	}
-
-	fmt.Println(m)
 
 	order := []string{"soil", "fertilizer", "water", "light", "temperature", "humidity", "location"}
 	for _, o := range order {
-		for k, v := range sm {
-			for _, i := range m[o] {
+		for k, v := range workingMap {
+			for _, i := range conf[o] {
 				if v >= i.start && v <= i.end {
-					sm[k] = i.sv + v - i.start
+					workingMap[k] = i.startValue + v - i.start
 				}
 			}
 		}
 	}
 
 	mx := math.MaxInt
-	for _, v := range sm {
+	for _, v := range workingMap {
 		if v < mx {
 			mx = v
 		}
@@ -83,50 +53,18 @@ func part1(input []string) interface{} {
 }
 
 func part2(input []string) interface{} {
-	m := make(map[string][]interval)
-
-	var seeds []int
-	to := ""
-	for _, v := range input {
-		if strings.Contains(v, "seeds:") {
-			parts := strings.Split(v, ": ")
-			for _, v := range strings.Split(parts[1], " ") {
-				is := conv.ToInt(v)
-				seeds = append(seeds, is)
-			}
-
-			continue
-		}
-		if strings.Contains(v, "map:") {
-			parts := strings.Split(v, " ")
-			ft := strings.Split(parts[0], "-to-")
-
-			to = ft[1]
-			continue
-		}
-
-		if v == "" {
-			continue
-		}
-
-		parts := strings.Split(v, " ")
-		destCoresp := conv.ToInt(parts[0])
-		sourceStart := conv.ToInt(parts[1])
-		size := conv.ToInt(parts[2])
-
-		m[to] = append(m[to], interval{sourceStart, sourceStart + size - 1, destCoresp})
-	}
+	seeds, conf := parseSeedsAndConfig(input)
 
 	mx := math.MaxInt
-	current := 0
 	order := []string{"soil", "fertilizer", "water", "light", "temperature", "humidity", "location"}
+	var current int
 	for i := 0; i < len(seeds); i += 2 {
 		for j := seeds[i]; j < seeds[i]+seeds[i+1]; j++ {
 			current = j
 			for _, o := range order {
-				for _, iv := range m[o] {
+				for _, iv := range conf[o] {
 					if current >= iv.start && current <= iv.end {
-						current = iv.sv + current - iv.start
+						current = iv.startValue + current - iv.start
 						break
 					}
 				}
@@ -139,4 +77,42 @@ func part2(input []string) interface{} {
 	}
 
 	return mx
+}
+
+func parseSeedsAndConfig(input []string) ([]int, map[string][]interval) {
+	m := make(map[string][]interval)
+	var seeds []int
+
+	for _, line := range input {
+		current := ""
+
+		switch {
+		case strings.Contains(line, "seeds:"):
+			parts := strings.Split(line, ": ")
+			for _, v := range strings.Split(parts[1], " ") {
+				is := conv.ToInt(v)
+				seeds = append(seeds, is)
+			}
+
+			continue
+		case strings.Contains(line, "map:"):
+			parts := strings.Split(line, " ")
+			ft := strings.Split(parts[0], "-to-")
+
+			current = ft[1]
+			continue
+
+		case line == "":
+			continue
+		}
+
+		parts := strings.Split(line, " ")
+		destCorresp := conv.ToInt(parts[0])
+		sourceStart := conv.ToInt(parts[1])
+		size := conv.ToInt(parts[2])
+
+		m[current] = append(m[current], interval{sourceStart, sourceStart + size - 1, destCorresp})
+	}
+
+	return seeds, m
 }
