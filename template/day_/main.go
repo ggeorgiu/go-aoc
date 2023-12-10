e5e7f705ae7e5eaec81fbb94b82a04edd71ffd52
ggeorgiu <ggeorgiu@uw.co.uk>
2023-12-10 13:18:58 +0200

    Update template

--- template/day_/main.go
+++ template/day_/main.go
@@ -7,15 +7,15 @@ import (
 )
 
 func main() {
-	input := in.ReadFileAsString("/year_/day_/part1.test")
+	input := in.ReadFileAsStringSlice("/year_/day_/part1.test")
 	fmt.Printf("P1: %v\n", part1(input))
 	// fmt.Printf("P2: %v\n", part2(input))
 }
 
-func part1(input string) interface{} {
+func part1(input []string) interface{} {
 	return ""
 }
 
-func part2(input string) interface{} {
+func part2(input []string) interface{} {
 	return ""
 }
